87d2a3c3252e59c375bf6d230ce8d69eda700bdb
denys <denispopovmaksimovich@gmail.com>
2022-10-14 21:46:32 +0300

    fix

--- works/DIM/public/js/script.js
+++ works/DIM/public/js/script.js
@@ -36,11 +36,11 @@ $('.help__button').click(function () {
 
 const popupLinks = document.querySelectorAll('.popup-link');
 const body = document.querySelector('body');
-const lockPadding = document.querySelectorAll(".lock-padding");
+
 
 let unlock = true;
 
-const timeout = 500;
+const timeout = 800;
 
 if (popupLinks.length > 0) {
 	for (let index = 0; index < popupLinks.length; index++) {
@@ -68,6 +68,7 @@ function popupOpen(curentPopup) {
 	if (curentPopup && unlock) {
 		const popupActive = document.querySelector('.popup.open');
 		if (popupActive) {
+
 			popupClose(popupActive, false);
 		} else {
 			bodyLock();
@@ -91,15 +92,6 @@ function popupClose(popupActive, doUnlock = true) {
 }
 
 function bodyLock() {
-	const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
-
-	if (lockPadding.length > 0) {
-		for (let index = 0; index < lockPadding.length; index++) {
-			const el = lockPadding[index];
-			el.style.paddingRight = lockPaddingValue;
-		}
-	}
-	body.style.paddingRight = lockPaddingValue;
 	body.classList.add('lock');
 
 	unlock = false;
@@ -110,13 +102,6 @@ function bodyLock() {
 
 function bodyUnLock() {
 	setTimeout(function () {
-		if (lockPadding.length > 0) {
-			for (let index = 0; index < lockPadding.length; index++) {
-				const el = lockPadding[index];
-				el.style.paddingRight = '0px';
-			}
-		}
-		body.style.paddingRight = '0px';
 		body.classList.remove('lock');
 	}, timeout);
 
@@ -134,7 +119,9 @@ document.addEventListener('keydown', function (e) {
 });
 
 (function () {
+
 	if (!Element.prototype.closest) {
+
 		Element.prototype.closest = function (css) {
 			var node = this;
 			while (node) {
@@ -146,15 +133,15 @@ document.addEventListener('keydown', function (e) {
 	}
 })();
 (function () {
+
 	if (!Element.prototype.matches) {
+
 		Element.prototype.matches = Element.prototype.matchesSelector ||
 			Element.prototype.webkitMatchesSelector ||
 			Element.prototype.mozMatchesSelector ||
 			Element.prototype.msMatchesSelector;
 	}
 })();
-
-
 
 $(document).ready(function () {
 	$('.cards-slider').slick({
