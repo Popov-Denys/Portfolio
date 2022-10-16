ecdb7516b0f6d18b586e148a00a6f4164a75bdc6
denys <denispopovmaksimovich@gmail.com>
2022-10-16 05:41:35 +0300

    new pages

--- works/DIM/public/js/script.js
+++ works/DIM/public/js/script.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function () {
+$(document).ready(function (e) {
 	const selectDefault = $(".header__dropdown > .header__list_default");
 
 	selectDefault.on("click", function () {
@@ -13,18 +13,19 @@ $(document).ready(function () {
 		$(this).addClass("active");
 		$(this).siblings().removeClass("active");
 		$(this).parents().removeClass("active");
-	});
-});
 
+	});
+
+});
 $(document).mouseup(function (e) {
 	$('.hamburger').click(function () {
 		$('.hamburger,.header__box,.header__menu').toggleClass('active');
-		$('body').toggleClass('lock');
+		$('body').toggleClass('locked');
 	});
 	var container = $(".header__box");
 	if (container.has(e.target).length === 0) {
 		$('.hamburger,.header__menu,.header__box').removeClass('active');
-		$('body').removeClass('lock');
+		$('body').removeClass('locked');
 	}
 });
 $('.introduction__button').click(function () {
@@ -33,7 +34,16 @@ $('.introduction__button').click(function () {
 $('.help__button').click(function () {
 	$(this).toggleClass('active');
 });
-
+$('.popup-map-activator').click(function () {
+	$(".popup-map").addClass('active');
+});
+$('.popup-map__cross').click(function () {
+	$(".popup-map").removeClass('active');
+});
+$('.form__button').click(function () {
+	$(".form__form").addClass('hidden');
+	$(".form__success").addClass('active');
+});
 const popupLinks = document.querySelectorAll('.popup-link');
 const body = document.querySelector('body');
 
@@ -142,6 +152,99 @@ document.addEventListener('keydown', function (e) {
 			Element.prototype.msMatchesSelector;
 	}
 })();
+
+var x, i, j, l, ll, selElmnt, a, b, c;
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+	selElmnt = x[i].getElementsByTagName("select")[0];
+	ll = selElmnt.length;
+	a = document.createElement("DIV");
+	a.setAttribute("class", "select-selected");
+	a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+	x[i].appendChild(a);
+	b = document.createElement("DIV");
+	b.setAttribute("class", "select-items select-hide");
+	for (j = 1; j < ll; j++) {
+		c = document.createElement("DIV");
+		c.innerHTML = selElmnt.options[j].innerHTML;
+		c.addEventListener("click", function (e) {
+			var y, i, k, s, h, sl, yl;
+			s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+			sl = s.length;
+			h = this.parentNode.previousSibling;
+			for (i = 0; i < sl; i++) {
+				if (s.options[i].innerHTML == this.innerHTML) {
+					s.selectedIndex = i;
+					h.innerHTML = this.innerHTML;
+					y = this.parentNode.getElementsByClassName("same-as-selected");
+					yl = y.length;
+					for (k = 0; k < yl; k++) {
+						y[k].removeAttribute("class");
+					}
+					this.setAttribute("class", "same-as-selected");
+					break;
+				}
+			}
+			h.click();
+		});
+		b.appendChild(c);
+	}
+	x[i].appendChild(b);
+	a.addEventListener("click", function (e) {
+		e.stopPropagation();
+		closeAllSelect(this);
+		this.nextSibling.classList.toggle("select-hide");
+		this.parentElement.classList.toggle("active");
+	});
+}
+
+function closeAllSelect(elmnt) {
+	var x, y, i, xl, yl, arrNo = [];
+	x = document.getElementsByClassName("select-items");
+	y = document.getElementsByClassName("select-selected");
+	xl = x.length;
+	yl = y.length;
+	for (i = 0; i < yl; i++) {
+		if (elmnt == y[i]) {
+			arrNo.push(i)
+		} else {
+			y[i].parentElement.classList.remove("active");
+		}
+	}
+	for (i = 0; i < xl; i++) {
+		if (arrNo.indexOf(i)) {
+			x[i].classList.add("select-hide");
+		}
+	}
+}
+document.addEventListener("click", closeAllSelect);
+
+$(document).ready(function () {
+	$('.team__slider').slick({
+		arrows: true,
+		dots: false,
+		adaptiveHeight: false,
+		outerEdgeLimit: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		waitForAnimate: true,
+		speed: 400,
+		variableWidth: true,
+		autoplay: false,
+		autoplaySpeed: 3000,
+		infinite: false,
+		draggable: true,
+		mobileFirst: true,
+		responsive: [
+			{
+				breakpoint: 1439,
+			}
+		]
+
+	});
+
+});
 
 $(document).ready(function () {
 	$('.cards-slider').slick({
