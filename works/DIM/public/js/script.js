90b7cab0e3266ebf3607d4f47c89eec1bfc01114
denys <denispopovmaksimovich@gmail.com>
2022-12-14 02:21:08 +0200

    update

--- works/DIM/public/js/script.js
+++ works/DIM/public/js/script.js
@@ -27,12 +27,8 @@ function reveal() {
 
 		if (elementTop < windowHeight - elementVisible) {
 			reveals[i].classList.add("active");
-		} else {
-			reveals[i].classList.remove("active");
 		}
-
 	}
-
 }
 
 window.addEventListener("scroll", reveal);
